b1638eae318bc4696f0ef7f1a7e4c52b6efba773
Hayden Westbrook <htw4@nau.edu>
2014-10-14 19:14:09 -0700

    Added basic operator code

---
+++ BasicOperators.py
@@ -0,0 +1,26 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def integer_divide(a, b):
+    return a // b
+
+def root(base, root):
+    return base ** (1/root)
+
+def power(base, power):
+    return base ** power
+
+def absolute_value(n):
+    if n < 0:
+        return n * -1
+    else:
+        return n
