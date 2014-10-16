878f23cf5e53db9445acc887da2c4414440bb57c
Hayden Westbrook <htw4@nau.edu>
2014-10-16 10:10:03 -0700

    Set up module

---
+++ calculator/formulas/formulas.py
@@ -0,0 +1,12 @@
+from ..operators import *
+
+
+def pythagorean(a, b, mode="h"):
+    if mode == "h":
+        return (a ** 2 + b ** 2) ** (1 / 2)
+    elif mode == "s":
+        return (b ** 2 - a ** 2) ** (1 / 2)
+
+
+def distance(x, y):
+    return absolute_value(pythagorean((x[1] - x[0]), (y[1] - y[0])))
