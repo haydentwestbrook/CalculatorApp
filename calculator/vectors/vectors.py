637619fcb3f532aa4f5fdf55fef589e2e00def6b
Hayden Westbrook <htw4@nau.edu>
2014-10-16 11:01:20 -0700

    Added more vector stuff

--- calculator/vectors/vectors.py
+++ calculator/vectors/vectors.py
@@ -12,8 +12,25 @@ class vector:
         else:
             raise TypeError()
 
+    def magnitude(self):
+        magnitude = 0
+        for value in self.values:
+            magnitude += value ** 2
+        magnitude ** (1/2)
+        self.magnitude = magnitude
+
+    def add(self, value):
+        try:
+            int(value)
+            self.values.append(value)
+        except:
+            raise TypeError()
+
     def __len__(self):
         return len(self.values)
+
+    def __contains__(self, item):
+        return self.values.__contains__(item)
 
     def __add__(self, other):
         if not isinstance(other, vector):
@@ -26,5 +43,35 @@ class vector:
                 new_values.append(self.values[i] + other.values[i])
             return vector(new_values)
 
-    
+    def __sub__(self, other):
+        if not isinstance(other, vector):
+            raise TypeError()
+        if len(self) != len(other):
+            raise Exception()
+        else:
+            new_values = []
+            for i in range(len(self)):
+                new_values.append(self.values[i] - other.values[i])
+            return vector(new_values)
 
+    def __mul__(self, other):
+        if not isinstance(other, vector):
+            raise TypeError()
+        if len(self) != len(other):
+            raise Exception()
+        else:
+            new_values = []
+            for i in range(len(self)):
+                new_values.append(self.values[i] * other.values[i])
+            return vector(new_values)
+
+    def __div__(self, other):
+        if not isinstance(other, vector):
+            raise TypeError()
+        if len(self) != len(other):
+            raise Exception()
+        else:
+            new_values = []
+            for i in range(len(self)):
+                new_values.append(self.values[i] / other.values[i])
+            return vector(new_values)
