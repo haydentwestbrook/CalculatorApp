9f98e5729b1ea3ef7092ff252a96e8835410a622
haydentwestbrook <haydentwestbrook@gmail.com>
2014-10-25 13:06:40 -0700

    Added dot and cross product to vectors

--- calculator/formulas.py
+++ calculator/formulas.py
@@ -6,20 +6,17 @@ from operators import subtract
 
 def pythagorean(a, b, mode="h"):
 
-    c = 0
-
     if mode == "h":
-        c = power(add(power(a,2),power(b,2)),.5)
+        c = power(add(power(a, 2), power(b, 2)), .5)
         #c = ((a ** 2 + b ** 2) ** (1 / 2))
     else:
-        c = power(absolute_value(subtract(power(b,2),power(a,2))),.5)
+        c = power(absolute_value(subtract(power(b, 2), power(a, 2))), .5)
         #c = ((b ** 2 - a ** 2) ** (1 / 2))
-
     return c
 
 
 def distance(x1, y1, x2, y2):
-    return absolute_value(pythagorean((x1 - x2), (y1 - y2)))
+    return absolute_value(pythagorean((subtract(x1, x2)), subtract(y1, y2)))
 
 
 def fib(x):
@@ -29,10 +26,10 @@ def fib(x):
     elif x == 1:
         return 1
     else:
-        return fib(x-1) + fib(x-2)
+        return add(fib(subtract(x, 1)), fib(subtract(x, 2)))
 
 
 def continuous_fib(x):
-    for i in range(x+1):
+    for i in range(add(x, 1)):
         print(fib(i))
     return fib(x)
--- calculator/vectors.py
+++ calculator/vectors.py
@@ -19,7 +19,7 @@ class Vector:
         magnitude ** (1/2)
         return magnitude
 
-    def add(self, value):
+    def append(self, value):
         try:
             int(value)
             self.values.append(value)
@@ -32,6 +32,15 @@ class Vector:
     def __contains__(self, item):
         return self.values.__contains__(item)
 
+    def __getitem__(self, i):
+        return self.values[i]
+
+    def __iter__(self):
+        return iter(self.values)
+
+    def __reversed__(self):
+        return iter(reversed(self.values))
+
     def __add__(self, other):
         if not isinstance(other, Vector):
             raise TypeError()
@@ -40,7 +49,7 @@ class Vector:
         else:
             new_values = []
             for i in range(len(self)):
-                new_values.append(self.values[i] + other.values[i])
+                new_values.append(self[i] + other[i])
             return Vector(new_values)
 
     def __sub__(self, other):
@@ -51,27 +60,47 @@ class Vector:
         else:
             new_values = []
             for i in range(len(self)):
-                new_values.append(self.values[i] - other.values[i])
+                new_values.append(self[i] - other[i])
             return Vector(new_values)
 
     def __mul__(self, other):
-        if not isinstance(other, Vector):
-            raise TypeError()
-        if len(self) != len(other):
-            raise Exception()
-        else:
+        try:
+            int(other)
             new_values = []
             for i in range(len(self)):
-                new_values.append(self.values[i] * other.values[i])
+                new_values.append(self[i] * other)
             return Vector(new_values)
+        except:
+            raise TypeError()
 
     def __div__(self, other):
-        if not isinstance(other, Vector):
-            raise TypeError()
-        if len(self) != len(other):
-            raise Exception()
-        else:
+        try:
+            int(other)
             new_values = []
             for i in range(len(self)):
-                new_values.append(self.values[i] / other.values[i])
-            return vector(new_values)
+                new_values.append(self[i] / other)
+            return Vector(new_values)
+        except:
+            raise TypeError()
+
+    def dot(self, vector_a, vector_b):
+        if not isinstance(vector_a, Vector) or not isinstance(vector_b, Vector):
+            raise TypeError()
+        if len(vector_a) != len(vector_b):
+            raise Exception()
+        else:
+            sum = 0
+            for i in range(len(vector_a)):
+                sum += vector_a[i] * vector_b[i]
+            return sum
+
+    def cross(self, vector_a, vector_b):
+        if not isinstance(vector_a, Vector) or not isinstance(vector_b, Vector):
+                raise TypeError()
+        if len(vector_a) != 3 and len(vector_b) != 3:
+            raise Exception()
+        else:
+            new_values = [vector_a.y * vector_b.z - vector_a.z * vector_b.y,
+                          vector_a.z * vector_b.x - vector_a.x * vector_b.z,
+                          vector_a.x * vector_b.y - vector_a.y * vector_b.x]
+            return Vector(new_values)
