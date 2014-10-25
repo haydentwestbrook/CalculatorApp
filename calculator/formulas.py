9a48d0b5695e2d415f25d10561946b7a6715d23f
alecdavidson <alecdavidson@nau.edu>
2014-10-24 22:12:06 -0700

    debugged formulas

--- calculator/formulas.py
+++ calculator/formulas.py
@@ -1,15 +1,25 @@
 from operators import absolute_value
+from operators import power
+from operators import add
+from operators import subtract
 
 
 def pythagorean(a, b, mode="h"):
+
+    c = 0
+
     if mode == "h":
-        return (a ** 2 + b ** 2) ** (1 / 2)
+        c = power(add(power(a,2),power(b,2)),.5)
+        #c = ((a ** 2 + b ** 2) ** (1 / 2))
     else:
-        return (b ** 2 - a ** 2) ** (1 / 2)
+        c = power(absolute_value(subtract(power(b,2),power(a,2))),.5)
+        #c = ((b ** 2 - a ** 2) ** (1 / 2))
+
+    return c
 
 
-def distance(x, y, w, z):
-    return absolute_value(pythagorean((x - w), (y - z)))
+def distance(x1, y1, x2, y2):
+    return absolute_value(pythagorean((x1 - x2), (y1 - y2)))
 
 
 def fib(x):
@@ -22,7 +32,7 @@ def fib(x):
         return fib(x-1) + fib(x-2)
 
 
-def continues_fib(x):
+def continuous_fib(x):
     for i in range(x+1):
         print(fib(i))
     return fib(x)
--- tempRun.py
+++ tempRun.py
@@ -1,3 +1,15 @@
-import calculator
+from calculator.formulas import pythagorean
 
-print(calculator.formulas.formulas.pythagorean(3,4))
+print(pythagorean(3, 4))
+
+print(pythagorean(3, 5, 'd'))
+
+print(pythagorean(5, 3, 'd'))
+
+
+from calculator.formulas import distance
+
+print(distance(0, 0, 2, 0))
+
+print(distance(1, 5, 1, 15))
+
