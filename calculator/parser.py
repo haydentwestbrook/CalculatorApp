dc3a883d2dcc6a54f7b5d7850c4ae692873087a5
Hayden <haydentwestbrook@gmail.com>
2014-11-14 00:00:12 -0700

    Finished pemdas for parser

--- calculator/parser.py
+++ calculator/parser.py
@@ -1,80 +1,40 @@
 from operators import *
 
-'''
-def parse2(string):
-    #Returns the answer to a mathematical
-    #expression inputed as a string
-    terms = get_terms(string)
-    if len(terms) == 1:
-        return terms[0]
-    #Builds queue containing all terms
-    #of the expression
-    queue = q.Queue()
-    queue.put(terms)
-    while len(terms) > 1:
-        terms = get_terms(terms[2])
-        queue.put(terms)
-    term = queue.get()
-    answer = term[0]
-    op = term[1]
-    #Evaluates the expression as values are 
-    #removed from the queue
-    while not queue.empty():
-        if op == "*":
-            start = term[0]
-            term_string = ""
-            while op == "*" and not queue.empty():
-                term = queue.get()
-                term_string += str(term[0])
-                if len(term) > 1:
-                    op = term[1]
-                    term_string += op
-                else:
-                    op = None
-            answer += start * parse(term_string)
-        elif op == "+":
-            term = queue.get()
-            answer = add(answer, term[0])
-            if len(term) > 1:
-                op = term[1]
-        elif op == "-":
-            term = queue.get()
-            answer = subtract(answer, term[0])
-            if len(term) > 1:
-                op = term[1]
-    return answer
-        
-    
-def get_terms(string):
-    #Returns a tuple with an int of the first
-    #number, the operator, and the remainder 
-    #of the expression string
-    if not isinstance(string, str):
-        raise TypeError()
-    if not len(string) > 0:
-        return None
-    i = 0
-    while True:
-        if string[0] == "-":
-            i += 1
-        try:
-            int(string[i])
-            i += 1
-        except:
-            break
-    a = int(string[0: i])
-    if i >= len(string) - 1:
-        return [a]
-    op = string[i]
-    if op == "/" and string[i+1] == "//":
-        op = "//"
-    if i+1 > len(string) - 1:
-        return [a]
-    b = string[i+1: len(string)]
-    return (int(a), op, b)
-'''
 
 def parse(string):
+
+    """
+    :param string:
+    :return string:
+
+    Takes in a string of math operations and returns the solution
+    """
+
+    i, found = 0, False
+    while i < len(string):
+        if string[i] == "(":
+            found = True
+            j = i + 1
+            while j < len(string) and string[j] != ")":
+                j += 1
+            new_string = string[i+1: j]
+            break
+        i += 1
+    if found:
+        string = string[0:i] + parse(new_string) + string[j+1:len(string)]
+
+    while "^" in string:
+        i, found = 0, False
+        while i < len(string):
+            if string[i] == "^":
+                found = True
+                break
+            i += 1
+        if found:
+            num1 = get_number(string, i, "b")
+            num2 = get_number(string, i, "f")
+            string = string[0:i-len(num1)] + str(float(num1) ** float(num2)) + string[i+len(num2)+1:len(string)]
+
     while "*" in string or "/" in string:
         i, found = 0, False
         while i < len(string):
@@ -92,12 +52,15 @@ def parse(string):
     while True:
         try:
             float(string)
+            if float(string) == float(int(float(string))):
+                string = string[0:len(string)-2]
             break
         except:
             pass
         i, found = 0, False
         while i < len(string):
-            if string[i] == "+" or string[i] == "-":
+            if string[i] == "+" or (string[i] == "-" and (i != 0 and not (string[i-1] == "+" or string[i-1] == "-" or
+                                                                          string[i-1] == "*" or string[i-1] == "/"))):
                 found = True
                 break
             i += 1
@@ -115,7 +78,7 @@ def get_number(string, i, mode="f"):
     if mode == "f":
         number = ""
         i += 1
-        if string[i] == "~":
+        if i < len(string) and string[i] == "-":
             number += string[i]
             i += 1
         while i < len(string):
@@ -141,9 +104,12 @@ def get_number(string, i, mode="f"):
                 i -= 1
             except:
                 break
-        if i >= 0 and string[i] == "~":
+        if string[i] == "-" and (i == 0 or (string[i-1] == "+" or string[i-1] == "-" or
+                                            string[i-1] == "*" or string[i-1] == "/")):
             number += string[i]
         number = number[::-1]
+    if number == "":
+        number = "0"
     return number
 
 
