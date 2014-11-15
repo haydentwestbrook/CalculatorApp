d2275aad69a10b8d47dbf4c15ede70fa0ef938b6
alecdavidson <alecdavidson@nau.edu>
2014-11-15 16:54:01 -0700

    Added basic web stuff! Run 'run.py' and go to '127.0.0.1:33507' in your browser.

--- server.py
+++ server.py
@@ -1,19 +1,23 @@
 ## Alec Davidson
 ## Hayden Westbrook
 ## Quentin Kantaris
-import flask, flask.views, os
+import flask
+import flask.views
+from calculator.parser import printer
 
 app = flask.Flask(__name__)
+app.secret_key = "DavidsonWestbrookKantaris"
 
 class View(flask.views.MethodView):
   def get(self):
     return flask.render_template('index.html')
-  
-  @property
+
   def post(self):
-    
+    equation = flask.request.form['equation']
+
+    printer(equation)
     return self.get()
   
-app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET','POST'])
+app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
 
 
